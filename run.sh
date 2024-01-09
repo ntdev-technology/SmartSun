93f87c8782fe7455e97507b7b3e5a4568c3834d9
Deadlkeyboard <niek.van.reenen@xs4all.nl>
2024-01-09 15:58:06 +0100

    update

--- run.sh
+++ run.sh
@@ -6,19 +6,24 @@ PATH_V1="./SmartSun-V1/main.py"
 PATH_V2="./SmartSun-V2"
 OPTIONS={"SmartSun versie 1","SmartSun versie 2","quit"}
 
+POSITIONAL_ARGS=()
 
-if [ -z "$TERM" ] && [ -z "$PS1" ]; then
-    echo "EZ-AUTOSTARTING $STANDARD"
-    
-    if [ "$STANDARD" == "SmartSun-V2" ]; then
-        cd "$PATH_V2"
-        gunicorn -b 0.0.0.0:80 wsgi:app --reload
-    elif [ "$STANDARD" == "SmartSun-V1" ]; then
-        python3.11 "$PATH_V1" -c
-    else
-        echo "Not a valid option for 'Standard'"
-    fi
-fi
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -c|--cron)
+            echo "Ez-autostarting $STANDARD - Crashless"
+            
+            if [ "$STANDARD" == "SmartSun-V2" ]; then
+                cd "$PATH_V2"
+                gunicorn -b 0.0.0.0:80 wsgi:app --reload
+            elif [ "$STANDARD" == "SmartSun-V1" ]; then
+                python3.11 "$PATH_V1" -c
+            else
+                echo "Not a valid option for 'Standard'"
+            fi
+        ;;
+    esac
+done
 
 if [ "$(id -u)" -ne 0 ];
   then echo "ERROR: Please run as root"
@@ -56,7 +61,10 @@ while true; do
             echo "Exitting SmartSun user prompt..."
             break;;
         *)
-            echo "Ooops, that's not an option!";;
+            echo "Ooops, that's not an option!"
+            echo "Exitting"
+            exit 1
+            ;;
     esac
 done
 
